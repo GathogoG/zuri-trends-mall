ec739e2e5bf3eb4614e7d8763540d7946f776208
George Gathogo <gathogogeorge11@gmail.com>
2024-08-06 12:54:13 +0300

    created init.py to setup python application factory

---
+++ Backend/app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from .extensions import db, migrate
+from .routes import main
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object('app.config.Config')
+    app.config.from_pyfile('config.py')
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    app.register_blueprint(main)
+
+    return app
