f1302a16a20f3394c4a579fc3c1c86bacb1930a6
George Gathogo <gathogogeorge11@gmail.com>
2024-08-06 14:00:36 +0300

    wrote CRUD methods for the user route

---
+++ Backend/app/routes/user.py
@@ -0,0 +1,44 @@
+from flask import Blueprint, request, jsonify
+from app.models import User
+from app.extensions import db
+
+user_bp = Blueprint('user_bp', __name__)
+
+@user_bp.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([user.as_dict() for user in users])
+
+@user_bp.route('/users/<int:id>', methods=['GET'])
+def get_user(id):
+    user = User.query.get_or_404(id)
+    return jsonify(user.as_dict())
+
+@user_bp.route('/users', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    user = User(
+        name=data['name'],
+        password=data['password'],
+        email=data['email']
+    )
+    db.session.add(user)
+    db.session.commit()
+    return jsonify(user.as_dict()), 201
+
+@user_bp.route('/users/<int:id>', methods=['PUT'])
+def update_user(id):
+    data = request.get_json()
+    user = User.query.get_or_404(id)
+    user.name = data['name']
+    user.password = data['password']
+    user.email = data['email']
+    db.session.commit()
+    return jsonify(user.as_dict())
+
+@user_bp.route('/users/<int:id>', methods=['DELETE'])
+def delete_user(id):
+    user = User.query.get_or_404(id)
+    db.session.delete(user)
+    db.session.commit()
+    return '', 204
