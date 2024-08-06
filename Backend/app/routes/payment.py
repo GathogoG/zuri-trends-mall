36a044da0f4cdd80cb57ab08e0addd60a51a63ca
George Gathogo <gathogogeorge11@gmail.com>
2024-08-06 15:01:16 +0300

    updated payment route to handle transaction ID

--- Backend/app/routes/payment.py
+++ Backend/app/routes/payment.py
@@ -1,19 +1,19 @@
-import requests
 from flask import Blueprint, request, jsonify
 from app.models import Payment
 from app.extensions import db
 from requests.auth import HTTPBasicAuth
+import requests
 import base64
 import json
 from datetime import datetime
 
 payment_bp = Blueprint('payment_bp', __name__)
 
+
 CONSUMER_KEY = 'yty83hjgw0EEGrxoV9j3AAQxVJL2hmjcvYMPxsjXH2ghL8AF'
 CONSUMER_SECRET = 'asJhwuTM0XXBWyTJwCWgPWITuucxPoDkNiQWfeTQGgjGraLyl5KO6Ay93sxrSwIm'
 BUSINESS_SHORT_CODE = '174379'
 LIPA_NA_MPESA_ONLINE_PASSKEY = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'
-PHONE_NUMBER = '0115743312'
 CALLBACK_URL = 'https://mydomain.com/path'
 
 
@@ -24,7 +24,7 @@ def get_access_token():
     return json_response['access_token']
 
 
-def lipa_na_mpesa_online(amount, phone_number):
+def lipa_na_mpesa_online(amount, phone_number, transaction_id):
     access_token = get_access_token()
     timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
     password = base64.b64encode((BUSINESS_SHORT_CODE + LIPA_NA_MPESA_ONLINE_PASSKEY + timestamp).encode()).decode('utf-8')
@@ -38,7 +38,7 @@ def lipa_na_mpesa_online(amount, phone_number):
         "PartyB": BUSINESS_SHORT_CODE,
         "PhoneNumber": phone_number,
         "CallBackURL": CALLBACK_URL,
-        "AccountReference": "ZuriTrends",
+        "AccountReference": transaction_id,  
         "TransactionDesc": "Payment for goods"
     }
     headers = {
@@ -67,24 +67,29 @@ def create_payment():
     data = request.get_json()
     amount = data['amount']
     phone_number = data['phone_number']
-
-
-    response = lipa_na_mpesa_online(amount, phone_number)
+    transaction_id = data['transaction_id']  
+    
+ 
+    response = lipa_na_mpesa_online(amount, phone_number, transaction_id)
+    payment_status = 'Pending'
     if response['ResponseCode'] == '0':
-        payment = Payment(
-            user_id=data['user_id'],
-            amount=amount,
-            payment_method='M-Pesa',
-            status='Pending'
-        )
-        db.session.add(payment)
-        db.session.commit()
-        return jsonify({
-            'payment': payment.as_dict(),
-            'mpesa_response': response
-        }), 201
+        payment_status = 'Successful'
     else:
-        return jsonify({'error': response['errorMessage']}), 400
+        payment_status = 'Failed'
+    
+    payment = Payment(
+        user_id=data['user_id'],
+        amount=amount,
+        transaction_id=transaction_id,
+        status=payment_status
+    )
+    db.session.add(payment)
+    db.session.commit()
+
+    return jsonify({
+        'payment': payment.as_dict(),
+        'mpesa_response': response
+    }), 201
 
 
 @payment_bp.route('/payments/<int:id>', methods=['PUT'])
@@ -93,7 +98,7 @@ def update_payment(id):
     payment = Payment.query.get_or_404(id)
     payment.user_id = data['user_id']
     payment.amount = data['amount']
-    payment.payment_method = data['payment_method']
+    payment.transaction_id = data['transaction_id']
     payment.status = data['status']
     db.session.commit()
     return jsonify(payment.as_dict())
