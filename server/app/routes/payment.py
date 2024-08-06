13b243f922c30a811e0a0e5b9b8fe4c36c308074
George Gathogo <gathogogeorge11@gmail.com>
2024-08-06 19:32:59 +0300

    Added phone number as a variable to the mpesa logic

--- server/app/routes/payment.py
+++ server/app/routes/payment.py
@@ -15,6 +15,7 @@ CONSUMER_SECRET = 'asJhwuTM0XXBWyTJwCWgPWITuucxPoDkNiQWfeTQGgjGraLyl5KO6Ay93sxrS
 BUSINESS_SHORT_CODE = '174379'
 LIPA_NA_MPESA_ONLINE_PASSKEY = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'
 CALLBACK_URL = 'https://mydomain.com/path'
+phonenumber ='0115743312'
 
 
 def get_access_token():
