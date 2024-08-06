17f58f7fd8cd985e919193ee77e20bf02e43986c
George Gathogo <gathogogeorge11@gmail.com>
2024-08-06 17:01:18 +0300

    fixed errors in manage and init.py

--- Server/app/__init__.py
+++ Server/app/__init__.py
@@ -1,15 +1,29 @@
 from flask import Flask
-from .extensions import db, migrate
-from .routes import register_blueprints
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-def create_app():
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object('app.config.Config')
-    app.config.from_pyfile('config.py')
+db = SQLAlchemy()
+migrate = Migrate()
 
+def create_app(config_class='config.Config'):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    
     db.init_app(app)
     migrate.init_app(app, db)
 
-    register_blueprints(app)
-
+    from .routes import catalog_bp, product_bp, user_bp, review_bp, wishlist_bp, cart_bp, payment_bp
+    app.register_blueprint(catalog_bp, url_prefix='/catalog')
+    app.register_blueprint(product_bp, url_prefix='/product')
+    app.register_blueprint(user_bp, url_prefix='/user')
+    app.register_blueprint(review_bp, url_prefix='/review')
+    app.register_blueprint(wishlist_bp, url_prefix='/wishlist')
+    app.register_blueprint(cart_bp, url_prefix='/cart')
+    app.register_blueprint(payment_bp, url_prefix='/payment')
+    
+    @app.before_first_request
+    def create_tables():
+        with app.app_context():
+            db.create_all()
+    
     return app
--- Server/app/manage.py
+++ Server/app/manage.py
@@ -1,10 +1,10 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from app import create_app, db
+from Server.app import create_app, db
 
 app = create_app()
-manager = Manager(app)
 migrate = Migrate(app, db)
+manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
 
