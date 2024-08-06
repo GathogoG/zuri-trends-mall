97fbf6eae91d5e1c9387f5bef3153d1de9da4f00
George Gathogo <gathogogeorge11@gmail.com>
2024-08-06 17:57:55 +0300

    made more changes to app.py to fix flask db init inssues

--- server/app/app.py
+++ server/app/app.py
@@ -9,7 +9,7 @@ migrate = Migrate()
 def create_app(config_class='server.config.Config'):
     app = Flask(__name__)
     app.config.from_object(config_class)
-    
+
     db.init_app(app)
     migrate.init_app(app, db)
 
