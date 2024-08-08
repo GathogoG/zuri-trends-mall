066b7a6feff9ca2ac38f788a4910f1286023ddf7
YvonneNdungu <ndunguyvonne10@gmail.com>
2024-08-08 14:36:13 +0300

    commit changes

--- Frontend/zuri-trends/src/pages/Wishlist.jsx
+++ Frontend/zuri-trends/src/pages/Wishlist.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
-import NavigationBar from '../components/NavigationBar'
+import NavigationBar from '../components/NavigationBar';
 
-function Wishlist() {
+
+const Wishlist = ({ wishlist, removeFromWishlist }) => {
   return (
     <div>
       <NavigationBar />
+      <h2>Wishlist</h2>
+      <ul>
+        {wishlist.map(product => (
+          <li key={product.id}>
+            {product.name} - ${product.price}
+            <button onClick={() => removeFromWishlist(product.id)}>Remove</button>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Wishlist
+export default Wishlist;
