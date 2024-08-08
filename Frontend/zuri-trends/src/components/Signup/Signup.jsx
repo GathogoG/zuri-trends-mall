5401a78360afb43dc1bf006a5c28a51dee3a8ae1
kennedy <kitongakennedy9@gmail.com>
2024-08-08 14:25:04 +0300

    fixed login and signup

---
+++ Frontend/zuri-trends/src/components/Signup/Signup.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./Signup.css";
+
+function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setError(null);
+    setSuccess(false);
+
+    const userData = {
+      email,
+      password,
+      name,
+    };
+
+    try {
+      const response = await fetch("http://127.0.0.1:5000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setSuccess(true);
+        console.log("User created:", data);
+        navigate("/login"); 
+      } else {
+        setError(data.error || "Something went wrong!");
+      }
+    } catch (err) {
+      console.error("Error:", err);
+      setError("Failed to sign up");
+    }
+  };
+
+  return (
+    <div className="signup-container">
+      <form onSubmit={handleSubmit}>
+        <h2>SIGN UP</h2>
+        {success && (
+          <p className="success-message">User created successfully!</p>
+        )}
+        {error && <p className="error-message">{error}</p>}
+        <div className="input-group">
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </div>
+        <div className="input-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="input-group">
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">SIGN UP</button>
+        <div className="extra-links">
+          <Link to="/login">Already have an account? Log in</Link>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
