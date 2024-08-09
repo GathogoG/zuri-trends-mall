fff1f3542f4d1cc65cf1a47aef81a6045db4c0bc
George Gathogo <gathogogeorge11@gmail.com>
2024-08-09 09:45:50 +0300

    Made changes on

--- server/app/routes/payment.py
+++ server/app/routes/payment.py
@@ -8,7 +8,6 @@ import json
 import string
 import random
 from datetime import datetime
-import uuid
 
 payment_bp = Blueprint('payment_bp', __name__)
 
@@ -18,6 +17,10 @@ BUSINESS_SHORT_CODE = '174379'
 LIPA_NA_MPESA_ONLINE_PASSKEY = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'
 CALLBACK_URL = 'https://yourdomain.com/path'
 COMPANY_NAME = 'Zuri-Trends'
+PHONE_NUMBER = '0115743312'
+
+def generate_transaction_id(length=12):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
 def get_access_token():
     try:
@@ -27,13 +30,14 @@ def get_access_token():
         json_response = response.json()
         return json_response['access_token']
     except requests.RequestException as e:
-        return jsonify({'error': str(e)}), 500
+        print(f"Failed to get access token: {str(e)}")
+        return None
 
 def lipa_na_mpesa_online(amount, phone_number, transaction_id):
     access_token = get_access_token()
-    if isinstance(access_token, dict):  
-        return access_token
-
+    if not access_token:
+        return {'error': 'Failed to get access token'}
+    
     timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
     password = base64.b64encode((BUSINESS_SHORT_CODE + LIPA_NA_MPESA_ONLINE_PASSKEY + timestamp).encode()).decode('utf-8')
     payload = {
@@ -47,7 +51,7 @@ def lipa_na_mpesa_online(amount, phone_number, transaction_id):
         "PhoneNumber": phone_number,
         "CallBackURL": CALLBACK_URL,
         "AccountReference": transaction_id,
-        "TransactionDesc": f"Payment to {COMPANY_NAME} for Transaction ID {transaction_id} and Amount KSh {amount}"
+        "TransactionDesc": f"{COMPANY_NAME} Payment for {transaction_id}"
     }
     headers = {
         "Authorization": f"Bearer {access_token}",
@@ -66,15 +70,13 @@ def lipa_na_mpesa_online(amount, phone_number, transaction_id):
 @payment_bp.route('/payments', methods=['POST'])
 def create_payment():
     data = request.get_json()
-    if not data or 'amount' not in data or 'phone_number' not in data:
+    if not data or not all(key in data for key in ['amount', 'phone_number']):
         return jsonify({'error': 'Invalid input'}), 400
-
-   
-    user_id = str(uuid.uuid4())
 
     amount = data['amount']
     phone_number = data['phone_number']
-    transaction_id = str(uuid.uuid4())  
+    transaction_id = generate_transaction_id()
+    user_id = generate_transaction_id()
 
     response = lipa_na_mpesa_online(amount, phone_number, transaction_id)
     if 'error' in response:
@@ -85,7 +87,7 @@ def create_payment():
         payment_status = 'Successful'
     else:
         payment_status = 'Failed'
-
+    
     payment = Payment(
         user_id=user_id,
         amount=amount,
