69455113d0fefb237f37a590bc5121262e3449a8
GathogoG <162293803+GathogoG@users.noreply.github.com>
2024-08-11 15:04:22 +0300

    Delete Frontend/zuri-trends directory

--- Frontend/zuri-trends/src/assets/utils/auth.jsx
+++
@@ -1,9 +0,0 @@
-// src/utils/auth.js
-
-export const logout = () => {
-  // Clear authentication token or user data
-  localStorage.removeItem('authToken'); // or sessionStorage.removeItem('authToken'); if you're using sessionStorage
-  
-  // Redirect to login page or home page
-  window.location.href = '/users'; // You can redirect to any page you'd like
-};
