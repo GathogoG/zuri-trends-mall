5f0c3fea665c45340a3df5a2377abf5d56512643
George Gathogo <gathogogeorge11@gmail.com>
2024-08-11 12:39:27 +0300

    Rewrote Navigation bar Product Manager and Product Manager components to fix errors

--- Frontend/zuri-trends/src/components/NavigationBar.jsx
+++ Frontend/zuri-trends/src/components/NavigationBar.jsx
@@ -1,69 +1,42 @@
-import React from 'react'
-import { Button, Col, Container, Form, Nav, Navbar, NavDropdown, Row } from 'react-bootstrap'
+import { Button, Container, Form, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 
-function NavigationBar() {
+const NavigationBar = () => {
   return (
-    <div className='navbar container-fluid navbar-expand-lg bg-body-tertiary'>
-        <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-      <Container >
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
         <Navbar.Brand href="/" className='nav-brand'>ZURI TRENDS</Navbar.Brand>
-        <Navbar.Brand href="/" className='nav-brand'>HOME</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <NavDropdown title="CATALOG" id="collapsible-nav-dropdown">
               <NavDropdown.Item href="/t-shirts">T-SHIRTS</NavDropdown.Item>
-              <NavDropdown.Item href="/croptops">
-                CROP-TOPS
-              </NavDropdown.Item>
+              <NavDropdown.Item href="/croptops">CROP-TOPS</NavDropdown.Item>
               <NavDropdown.Item href="/hoodies">HOODIES</NavDropdown.Item>
-              <NavDropdown.Item href="/jackets">
-                JACKETS
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/crophoodies">
-                CROP-HOODIES
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/sweatshirts">
-                SWEAT-SHIRTS
-              </NavDropdown.Item>
+              <NavDropdown.Item href="/jackets">JACKETS</NavDropdown.Item>
+              <NavDropdown.Item href="/crophoodies">CROP-HOODIES</NavDropdown.Item>
+              <NavDropdown.Item href="/sweatshirts">SWEAT-SHIRTS</NavDropdown.Item>
             </NavDropdown>
             <Nav.Link href="/contact">CONTACT</Nav.Link>
           </Nav>
-          <Nav>
-          <Navbar.Brand href="/" className='nav'>ZURI TRENDS</Navbar.Brand>
-          </Nav>
-          <Nav>
+          <Nav className="ms-auto">
             <Nav.Link href="/about">ABOUT US</Nav.Link>
-            <Nav.Link eventKey={2} href="/cart">
-              CART
-            </Nav.Link>
-            <Nav.Link eventKey={2} href="/wishlist">
-              WISHLIST
-            </Nav.Link>
+            <Nav.Link href="/cart">CART</Nav.Link>
+            <Nav.Link href="/wishlist">WISHLIST</Nav.Link>
             <Nav.Link href="/logout">LOGOUT</Nav.Link>
-            <Form inline="true">
-            <Nav.Link eventKey={2} href="/orderhistory">
-              ORDER HISTORY
-            </Nav.Link>
-        <Row>
-          <Col xs="auto">
+            <Nav.Link href="/orderhistory">ORDER HISTORY</Nav.Link>
+          </Nav>
+          <Form className="d-flex ms-auto">
             <Form.Control
               type="text"
               placeholder="Search"
-              className=" mr-sm-2"
+              className="me-2"
             />
-          </Col>
-          <Col xs="auto">
             <Button type="submit" className='submit'>Submit</Button>
-          </Col>
-        </Row>
-      </Form>
-          </Nav>
+          </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-    </div>
-  )
+  );
 }
 
-export default NavigationBar
+export default NavigationBar;
