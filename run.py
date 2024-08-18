10d6244ee1f36b470621d5ce2a283bb8b3a7599f
Yvonne-Ndungu <162134968+IVON1010@users.noreply.github.com>
2024-08-18 22:09:41 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,1 @@
-from app import create_app
 
-app = create_app()
-
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) > 1 and sys.argv[1] == 'runserver':
-        app.run(debug=True)
-    else:
-        print("Usage: python run.py runserver")
